020f3cc35bf34f6439728c20f9a466ace294bfe4
AMIT KUMAR <amtvmpire@gmail.com>
2018-08-10 01:51:07 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+'''''''''
+Project: Digit recognition using sigmoid neural network using MNIST data from SCRATCH!
+AMIT KUMAR
+IIT(ISM) Dhanbad
+CSE(B.tech)
+'''''''''
+
+import NeuralNetwork
+import LoadData
+
+training_data, validation_data, test_data = LoadData.ProcessData()
+neuron = NeuralNetwork.SigmoidNeuralNetwork([784, 30, 10])
+neuron.SGD(training_data, 25, 10, 3.0, test_data = test_data)
