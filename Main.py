c895fc2673d32c9b002975fc8d66f65145efa167
AMIT KUMAR <amtvmpire@gmail.com>
2018-08-19 18:49:54 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,4 +10,5 @@ import LoadData
 
 training_data, validation_data, test_data = LoadData.ProcessData()
 neuron = NeuralNetwork.SigmoidNeuralNetwork([784, 30, 10])
-neuron.SGD(training_data, 25, 10, 3.0, test_data = test_data)
+neuron.MBGD(training_data, 25, 10, 3.0, test_data = test_data)
+#mini-batch gradient descent
